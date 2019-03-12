95804f68e6f336ca9ecf04d75c1c49cdc532fabb
Jason <jason@jasons-mbp-4.lan>
2019-03-12 13:11:27 -0400

    more minimom

--- scripts/configure-k8s.py
+++ scripts/configure-k8s.py
@@ -3,23 +3,21 @@
 import os
 import sys
 from jinja2 import Template
-
+from dotenv import load_dotenv
 #export OM_HOST=http://localhost:8080
 #export OM_USER=admin
 #export OM_PASSWORD=admin12345%
 #export OM_API_KEY=1dcf1b5a-a35b-454f-851d-62cf47ee266c
 
 
+load_dotenv("/etc/mongodb/mms/env/.ops-manager-env")
 parameters = {}
 keys = [ "OM_HOST", "OM_USER", "OM_PASSWORD", "OM_API_KEY" ]
 for key in keys:
   parameters[key]=os.environ[key]
 
-if sys.argv[1]:
-  template_file=sys.argv[1]
-else :
-  path="/etc/mongodb/mms/env"
-  template_file="%s/k8s_config_template.yaml" % path
+path="/opt/scripts"
+template_file="%s/k8s_config_template.yaml" % path
 
 with open(template_file, 'r') as t:
   template = t.read()
